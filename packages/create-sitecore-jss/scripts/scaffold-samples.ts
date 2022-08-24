18249cf75027c3f14f7bc27d30e776c04a58673b
Artem Alexeyenko <ala@sitecore.net>
2022-08-24 16:43:06 -0400

    pipeline and scaffold update

--- packages/create-sitecore-jss/scripts/scaffold-samples.ts
+++ packages/create-sitecore-jss/scripts/scaffold-samples.ts
@@ -1,58 +1,20 @@
 import chalk from 'chalk';
 import { initRunner } from '../src/init-runner';
-const templates = require ('./templates.json');
+// const templates = require('./templates.json');
+import samplesToSacffold from './samples.json';
 
-type SampleArgs = {
-  initializers: string[];
-  args: {
-    appName: string;
-    destination: string;
-    fetchWith: string;
-    prerender: string;
-  }
-}
-
-function transformArgs(inputArgs: SampleArgs, sampleName: string, sampleInitializers: string[]): SampleArgs {
-  let outputArgs: SampleArgs = {
-    initializers: sampleInitializers,
-    args: Object.assign({}, inputArgs.args)
-  };
-  outputArgs.args.appName = sampleName;
-  outputArgs.args.destination = outputArgs.args.destination.replace(/\\\w+$/, `\\${sampleName}`);
-  return outputArgs;
-}
-
-const nextjsPermutations:Record<string,string[]> = {
-  "nextjs-vanilla":['nextjs'], 
-  "nextjs-sxa":['nextjs', 'nextjs-sxa'], 
-  "nextjs-personalize-styleguide":['nextjs', 'nextjs-styleguide', 'nextjs-personalize']
-};
-const samplesToInstall = ['nextjs', 'react', 'angular', 'vue'];
-
-let samples = new Array<SampleArgs>();
-
-//build samples info
-
-for (const sample of samplesToInstall) {
-  if (sample == 'nextjs') {
-    for (const permutation in nextjsPermutations){
-      samples.push(transformArgs(templates.nextjs, permutation, nextjsPermutations[permutation]));
-    }
-  } else {
-    samples.push(transformArgs(templates.default, sample, [sample]));
-  }
-}
-
-
-for (const sample of samples) {
+for (const sample of samplesToSacffold) {
   console.log(chalk.green(`Initializing sample ${sample.args.appName} ...`));
-  initRunner(sample.initializers, {
+  // we need to keep noInstall as true - otherwise both yarn install and lint will execute
+  // we run lint separately in the azure pipeline
+  let scaffoldArgs = {
     ...sample.args,
     templates: sample.initializers,
     yes: true,
     force: true,
     silent: true,
     noInstall: true,
-  });
+  }
+  initRunner(sample.initializers, scaffoldArgs);
 }
 
