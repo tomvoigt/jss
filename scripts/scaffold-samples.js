107b6f758f4ff9030acdc7770fe6706ae0561748
Artem Alexeyenko <ala@sitecore.net>
2022-08-29 19:45:59 -0400

    refactor, shuffle test

---
+++ scripts/scaffold-samples.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+const { initRunner } = require('../packages/create-sitecore-jss/src/init-runner');
+const samplesToSacffold = require('./samples.json');
+
+for (const sample of samplesToSacffold) {
+  sample.args.appName = `sample-${sample.args.appName}`;
+  console.log(chalk.green(`Initializing sample ${sample.args.appName} ...`));
+  // we need to keep noInstall as true - otherwise both yarn install and lint will execute
+  // we run lint separately in the azure pipeline
+  let scaffoldArgs = {
+    ...sample.args,
+    templates: sample.initializers,
+    yes: true,
+    force: true,
+    silent: true,
+    noInstall: true,
+  }
+  initRunner(sample.initializers, scaffoldArgs);
+}
+
