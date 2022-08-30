dd6cbec72648b9b3315524f9a67e6a4a9b61c50a
Artem Alexeyenko <ala@sitecore.net>
2022-08-30 15:15:45 -0400

    alt path test

--- scripts/scaffold-samples.js
+++ scripts/scaffold-samples.js
@@ -4,7 +4,7 @@ const samplesToSacffold = require('./samples.json');
 
 for (const sample of samplesToSacffold) {
   sample.args.appName = `sample-${sample.args.appName}`;
-  sample.args.destination = `.\\samples\\${sample.args.appName}`;
+  sample.args.destination = `./samples/${sample.args.appName}`;
   console.log(chalk.green(`Initializing sample ${sample.args.appName} ...`));
   // we need to keep noInstall as true - otherwise both yarn install and lint will execute
   // we run lint separately in the azure pipeline
